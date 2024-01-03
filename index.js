1ebdbe4dd37d81db4da0211cf402d40fe8a90e44
anne <anneirungu07@gmail.com>
2024-01-03 16:41:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+console.log (multiply);
+
+const random = Math.floor(Math.random() *1000000000000) + 1;
+console.log(random)
+
+const num3 = 7
+const num4 = 32
+const mod = num4 % num3
+console.log (mod)
+
+let numberSequence =(1,7,5,8,7,19,8,15,20)
+const max = (Math.max (numberSequence))
+console.log (max)
